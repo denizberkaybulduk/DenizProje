eed7b0744a69e711c0ce632b65731706b5458855
Deniz Berkay <denizberkay@Deniz-MacBook-Pro.local>
2025-07-16 12:00:28 +0300

    login_controller olusturuldu

---
+++ lib/Controller/login_controller.dart
@@ -0,0 +1,26 @@
+import 'package:get/get.dart';
+import '../Service/login_service.dart';
+
+class LoginController extends GetxController {
+  final LoginService _service = LoginService();
+
+  var email = ''.obs;
+  var password = ''.obs;
+  var isLoading = false.obs;
+  var loginError = ''.obs;
+
+  Future<void> login() async {
+    isLoading.value = true;
+    loginError.value = '';
+
+    bool success = await _service.login(email.value, password.value);
+
+    if (success) {
+      // Login başarılı, anasayfaya yönlendirme yapabiliriz
+      Get.offNamed('/users'); // örnek rota
+    } else {
+      loginError.value = "Giriş başarısız. Lütfen bilgilerinizi kontrol edin.";
+    }
+    isLoading.value = false;
+  }
+}
