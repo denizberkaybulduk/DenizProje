9d0b58f2f85c7c24b6ef94365be3ec31e663d8b1
yigit.yilmaz <yigit.yilmaz@icrontech.com>
2025-07-22 11:14:10 +0300

    ...

--- lib/Service/api/user_service.dart
+++ lib/Service/api/user_service.dart
@@ -7,7 +7,12 @@ class UserService {
 
   Future<List<User>> fetchUsers() async {
     try {
-      final response = await _client.get('/users?page=1');
+      final response = await _client.getWithWholeUrl(
+          'https://api.jsonbin.io/v3/b/687f44cc7b4b8670d8a53fa2', {
+        'Content-Type': 'application/json',
+        'X-Master-Key':
+            '\$2a\$10\$qgX6xNGshFij5SNdn..5dujiYw/H5QDoAa9xY0bFPSLnpo3/Hf2yC',
+      });
 
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
